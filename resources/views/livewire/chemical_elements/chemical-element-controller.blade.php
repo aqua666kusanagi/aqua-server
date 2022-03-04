b7bef59240b6300a5e11776d064cbd63ba762e12
mike_828 <miaxher@hotmail.com>
2022-03-03 22:36:53 -0600

    conflict solved

--- resources/views/livewire/chemical_elements/chemical-element-controller.blade.php
+++ resources/views/livewire/chemical_elements/chemical-element-controller.blade.php
@@ -1,8 +1,10 @@
 @extends('layouts.template')
 @section('content')
+
 <div class="py-12">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg px-4 py-4">
+            <!-- ALgo de seccion-->
             @if (session()->has('message'))
                 <div class="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md my-3"
                      role="alert">
@@ -14,7 +16,7 @@
                 </div>
             @endif
             <button wire:click="create()"
-                    class="bg-indigo-500 text-white font-bold py-2 px-4 rounded my-3"><i class="fa-solid fa-plus"></i> Elementos Quimicos</button>
+                    class="bg-primary text-white font-bold py-2 px-4 rounded my-3"><i class="fa-solid fa-plus"></i> Elementos Quimicos</button>
             @if($isDialogOpen)
                 @include('livewire.chemical_elements.create')
             @endif
@@ -24,7 +26,6 @@
                     <th class="px-4 py-2 w-20">No.</th>
                     <th class="px-4 py-2">Name</th>
                     <th class="px-4 py-2">Chemical Code</th>
-
                     <th class="px-4 py-2">Action</th>
                 </tr>
                 </thead>
@@ -42,6 +43,9 @@
                                     class="bg-red-700 text-white font-bold py-2 px-4"><i class="fa-solid fa-trash-can"></i></button>
                         </td>
                     </tr>
+                @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
