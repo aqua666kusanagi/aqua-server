cb5e1f43bfee3618c77dccce3d056745a2db73fb
Juan Diego Estrada <jonycursos21@gmail.com>
2022-03-05 21:51:23 -0600

    Modifiqued Loging

--- resources/views/livewire/chemical_elements/create.blade.php
+++ resources/views/livewire/chemical_elements/create.blade.php
@@ -1,5 +1,4 @@
-@extends('layouts.app')
-@section('content')
+
 <div class="fixed z-10 inset-0 overflow-y-auto ease-out duration-400">
     <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 sm:block sm:p-0">
         <div class="fixed inset-0 transition-opacity">
@@ -39,4 +38,4 @@
         </div>
     </div>
 </div>
-@endsection
+
