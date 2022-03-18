b13eb60a518f11ba4bf9a5bd296b4ab2285aed04
Juan Diego Estrada <jonycursos21@gmail.com>
2022-03-18 13:51:41 -0600

    create update

--- resources/views/livewire/application_modes/create.blade.php
+++ resources/views/livewire/application_modes/create.blade.php
@@ -1,7 +1,7 @@
 <div class="fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
-    <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+    <div class="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
 
-        <div class="fixed inset-0 bg-gray-500 bg-opacity-25 transition-opacity " wire:click="closeModalPopover()" aria-hidden="true"></div>
+        <div class="fixed inset-0 bg-black opacity-50 transition-opacity " wire:click="closeModalPopover()" aria-hidden="true"></div>
 
         <!-- This element is to trick the browser into centering the modal contents. -->
         <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
@@ -9,11 +9,12 @@
 
         <div class="relative inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6">
             <div>
-                <div class="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100">
+                <div class="mx-auto flex items-center justify-center h-12 w-48 rounded-full bg-gray-100">
                     <!-- Heroicon name: outline/check -->
-                    <svg class="h-6 w-6 text-green-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                    {{--<svg class="h-6 w-6 text-green-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
-                    </svg>
+                    </svg>--}}
+                    <h3 class="items-center">NUEVO MODOS DE APLICACION</h3>
                 </div>
                 <div class="mt-3 text-center sm:mt-5">
                     <div class="mt-2">
