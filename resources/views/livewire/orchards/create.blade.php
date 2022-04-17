a457e6cbe8526bfa2f508e855ac515ff413b521c
Miguel Axel Cejudo Hernandez <miaxher.828@gmail.com>
2022-04-17 01:32:30 -0500

    Orchard update

--- resources/views/livewire/orchards/create.blade.php
+++ resources/views/livewire/orchards/create.blade.php
@@ -6,25 +6,27 @@
         <span class="hidden sm:inline-block sm:align-middle sm:h-screen"></span>
 
 
-        <div class="relative inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle  w-full">
-            
-            <div class="">
+        <div class="relative  inline-block align-bottom bg-white  text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle  w-full">
+
+            <div class="px-4">
 
 
-
-                <div class="mx-auto flex items-center justify-center h-12 w-full rounded-full bg-green-100 shadow-lg ">
-                    <h3 class="items-center">HUERTO</h3>
+                <div class=" ">
+                    <div class="mt-6 border-b lg:py-6 flex items-center justify-center h-12 w-full rounded-full ring-4 ring-green-500 ring-opacity-50 shadow-lg ">
+                        <h3 class="items-center">HUERTO</h3>
+                    </div>
                 </div>
 
-                <div class="md:grid md:grid-cols-3 md:gap-6    px-4   ">
+
+                <div class="mb-8  md:grid md:grid-cols-3 md:gap-6  mt-6 border-b lg:py-6   px-6  ring-4 ring-green-500 ring-opacity-50 ">
 
 
 
 
-                    <div class="border-b lg:py-6 dark:border-primary-darker">
+                    <div class="   ">
                         <div class="pt-2 pr-4 flex">
                             <h3 class="block text-sm font-medium text-gray-700 pr-4">Tipo de Aguacate</h3>
-                            <select wire:model="type_avocado_id" class="sshadow appearance-none border rounded-full ">
+                            <select wire:model="type_avocado_id" class="sshadow appearance-none border rounded-full w-full ">
                                 <option value="">--seleccionar--</option>
                                 @foreach($type_avocados as $type_avocado)
                                 <option type="int" value="{{$type_avocado->id}}">{{$type_avocado->type_avocado}}
@@ -34,10 +36,10 @@
                         </div>
                     </div>
 
-                    <div class="border-b lg:py-6 dark:border-primary-darker">
+                    <div class="">
                         <div class="pt-2 pr-4 flex">
                             <h3 class="block text-sm font-medium text-gray-700 pr-4">Tipo de Topografia</h3>
-                            <select wire:model="type_topography_id" class="sshadow appearance-none border rounded-full ">
+                            <select wire:model="type_topography_id" class="sshadow appearance-none border rounded-full w-full">
                                 <option value="">--seleccionar--</option>
                                 @foreach($type_topographies as $type_topopraphy)
                                 <option type="int" value="{{$type_topopraphy->id}}">{{$type_topopraphy->type_topography}}
@@ -48,10 +50,10 @@
                         </div>
                     </div>
 
-                    <div class="border-b lg:py-6 dark:border-primary-darker">
+                    <div class="">
                         <div class="pt-2 pr-4 flex">
                             <h3 class="block text-sm font-medium text-gray-700 pr-4">Tipo de Suelo</h3>
-                            <select wire:model="type_soil_id" class="sshadow appearance-none border rounded-full ">
+                            <select wire:model="type_soil_id" class="sshadow appearance-none border rounded-full w-full">
                                 <option value="">--seleccionar--</option>
                                 @foreach($type_soils as $type_soil)
                                 <option type="int" value="{{$type_soil->id}}">{{$type_soil->type_soil}}
@@ -67,10 +69,10 @@
 
 
 
-                    <div class="py-2 shadow-lg rounded-lg">
+                    <div class="">
                         <div class="pt-2 pr-4 flex">
                             <h3 class="block text-sm font-medium text-gray-700 pr-4">Tipo de Clima</h3>
-                            <select wire:model="climate_type_id" class="sshadow appearance-none border rounded-full ">
+                            <select wire:model="climate_type_id" class="sshadow appearance-none border rounded-full w-full">
                                 <option value="">--seleccionar--</option>
                                 @foreach($climate_types as $climate_type)
                                 <option type="int" value="{{$climate_type->id}}">{{$climate_type->climate_type}}
@@ -81,10 +83,10 @@
                     </div>
 
 
-                    <div class="py-2 shadow-lg rounded-lg">
+                    <div class="">
                         <div class="pt-2 pr-4 flex">
                             <h3 class="block text-sm font-medium text-gray-700 pr-4">Usuario</h3>
-                            <select wire:model="user_id" class="sshadow appearance-none border rounded-full ">
+                            <select wire:model="user_id" class="sshadow appearance-none border rounded-full w-full">
                                 <option value="">--seleccionar--</option>
                                 @foreach($users as $users)
                                 <option type="int" value="{{$users->id}}">{{$users->name}}
@@ -101,11 +103,11 @@
 
 
 
-                    <div class="py-2 shadow-lg rounded-lg">
+                    <div class="">
                         <div class="pt-2 pr-4">
                             <h3 class="block text-sm font-medium text-gray-700">Nombre del Huerto</h3>
                             <div class="">
-                                <input type="text" class="sshadow appearance-none border  rounded-full" placeholder="Nombre de Huerto" wire:model="name_orchard">
+                                <input type="text" class="sshadow appearance-none border  rounded-full w-full" placeholder="Nombre de Huerto" wire:model="name_orchard">
                             </div>
                             @error('name_orchard') <span class="text-red-500">{{ $message }}</span>@enderror
                         </div>
@@ -113,7 +115,7 @@
 
 
 
-                    <div class="py-2 shadow-lg rounded-lg">
+                    <div class="">
                         <label class="block text-sm font-medium text-gray-700"> Cargar Imagen </label>
                         <div class="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
                             <div class="space-y-1 text-center">
@@ -133,94 +135,94 @@
                         </div>
                     </div>
 
-                    <div class="py-2 shadow-lg rounded-lg">
+                    <div class="">
                         <div class="pt-2 pr-4">
                             <h3 class="block text-sm font-medium text-gray-700">Ubicacion</h3>
                             <div class="">
-                                <input type="text" class="sshadow appearance-none border  rounded-full" placeholder="Localizacion de Huerto" wire:model="location_orchard">
+                                <input type="text" class="sshadow appearance-none border  rounded-full w-full" placeholder="Localizacion de Huerto" wire:model="location_orchard">
                             </div>
                             @error('location_orchard') <span class="text-red-500">{{ $message }}</span>@enderror
                         </div>
                     </div>
 
-                    <div class="py-2 shadow-lg rounded-lg">
+                    <div class="">
                         <div class="pt-2 pr-4">
                             <h3 class="block text-sm font-medium text-gray-700">Punto</h3>
                             <div class="">
-                                <input type="text" class="sshadow appearance-none border  rounded-full" placeholder="Point" wire:model="point">
+                                <input type="text" class="sshadow appearance-none border  rounded-full w-full" placeholder="Point" wire:model="point">
                             </div>
                             @error('point') <span class="text-red-500">{{ $message }}</span>@enderror
                         </div>
                     </div>
 
-                    <div class="py-2 shadow-lg rounded-lg">
+                    <div class="">
                         <div class="pt-2 pr-4">
                             <h3 class="block text-sm font-medium text-gray-700">Area</h3>
                             <div class="">
-                                <input type="text" class="sshadow appearance-none border  rounded-full" placeholder="Area" wire:model="area">
+                                <input type="text" class="sshadow appearance-none border  rounded-full w-full" placeholder="Area" wire:model="area">
                             </div>
                             @error('area') <span class="text-red-500">{{ $message }}</span>@enderror
                         </div>
                     </div>
 
 
-                    <div class="py-2 shadow-lg rounded-lg">
+                    <div class="">
                         <div class="pt-2 pr-4">
                             <h3 class="block text-sm font-medium text-gray-700">Altitud</h3>
                             <div class="">
-                                <input type="text" class="sshadow appearance-none border  rounded-full" placeholder="Altitud" wire:model="altitude">
+                                <input type="text" class="sshadow appearance-none border  rounded-full w-full" placeholder="Altitud" wire:model="altitude">
                             </div>
                             @error('altitude') <span class="text-red-500">{{ $message }}</span>@enderror
                         </div>
                     </div>
 
 
-                    <div class="py-2 shadow-lg rounded-lg">
+                    <div class="">
                         <div class="pt-2 pr-4">
                             <h3 class="block text-sm font-medium text-gray-700">Superficie</h3>
                             <div class="">
-                                <input type="text" class="sshadow appearance-none border  rounded-full" placeholder="Superficie" wire:model="surface">
+                                <input type="text" class="sshadow appearance-none border  rounded-full w-full" placeholder="Superficie" wire:model="surface">
                             </div>
                             @error('surface') <span class="text-red-500">{{ $message }}</span>@enderror
                         </div>
                     </div>
 
-                    <div class="py-2 shadow-lg rounded-lg">
+                    <div class="">
                         <div class="pt-2 pr-4">
                             <h3 class="block text-sm font-medium text-gray-700">Estado</h3>
                             <div class="">
-                                <input type="text" class="sshadow appearance-none border  rounded-full" placeholder="Estado" wire:model="state">
+                                <input type="text" class="sshadow appearance-none border  rounded-full w-full" placeholder="Estado" wire:model="state">
                             </div>
                             @error('state') <span class="text-red-500">{{ $message }}</span>@enderror
                         </div>
                     </div>
 
-                    <div class="py-2 shadow-lg rounded-lg">
+                    <div class="">
                         <div class="pt-2 pr-4">
                             <h3 class="block text-sm font-medium text-gray-700">Año de Creacion</h3>
                             <div class="">
-                                <input type="text" class="sshadow appearance-none border  rounded-full" placeholder="Año de Creacion" wire:model="creation_year">
+                                <input type="text" class="sshadow appearance-none border  rounded-full w-full" placeholder="Año de Creacion" wire:model="creation_year">
                             </div>
                             @error('creation_year') <span class="text-red-500">{{ $message }}</span>@enderror
                         </div>
                     </div>
 
-                    <div class="py-2 shadow-lg rounded-lg">
+                    <div class="">
                         <div class="pt-2 pr-4">
                             <h3 class="block text-sm font-medium text-gray-700">Densidad de plantado</h3>
                             <div class="">
-                                <input type="text" class="sshadow appearance-none border  rounded-full" placeholder="Densidad de Plantado" wire:model="planting_density">
+                                <input type="text" class="sshadow appearance-none border  rounded-full w-full" placeholder="Densidad de Plantado" wire:model="planting_density">
                             </div>
                             @error('planting_density') <span class="text-red-500">{{ $message }}</span>@enderror
                         </div>
                     </div>
 
 
-                    <div class="py-2 shadow-lg rounded-lg">
+                    <div class="">
                         <div class="pt-2 pr-4">
                             <h3 class="block text-sm font-medium text-gray-700">Irrigacion</h3>
                             <div class="">
-                                <input type="text" class="sshadow appearance-none border  rounded-full" placeholder="Irrigacion" wire:model="irrigation">
+                                <input type="text" class="sshadow appearance-none border  rounded-full w-full" placeholder="Irrigacion" wire:model="irrigation">
                             </div>
                             @error('irrigation') <span class="text-red-500">{{ $message }}</span>@enderror
                         </div>
@@ -234,7 +236,7 @@
                 </div>
 
 
-                <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                <div class="bg-gray-50 px-4 py-3 sm:px-6 flex">
                     <span class="flex w-full rounded-md shadow-sm sm:ml-3 sm:w-auto">
                         <button wire:click.prevent="store()" type="button" class="inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-red-600 text-base leading-6 font-bold text-white shadow-sm hover:bg-red-700 focus:outline-none focus:border-green-700 focus:shadow-outline-green transition ease-in-out duration-150 sm:text-sm sm:leading-5">
                             Guardar
@@ -250,6 +252,8 @@
 
 
             </div>
+
+
         </div>
     </div>
 </div>
