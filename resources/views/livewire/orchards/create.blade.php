984749977afa0c3ccfeaff535049e3410659088c
Juan Diego Estrada <jonycursos21@gmail.com>
2022-05-13 14:16:43 -0500

    orchard updated

--- resources/views/livewire/orchards/create.blade.php
+++ resources/views/livewire/orchards/create.blade.php
@@ -108,13 +108,14 @@
                                             </div>
                                             --}}
 
-
+                                        Mensage:{{$path_image}}<br>
+                                        Bandera:{{file_exists($path_image)}}
                                         <div class="rounded-lg col-span-6  ">
                                             <label class="text-center block text-sm font-medium text-gray-700"> Cargar Imagen </label>
                                             <div class="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md ">
                                                 <div class="space-y-1 text-center items-center">
                                                     @if ($path_image)
-                                                    <img src="{{$path_image->TemporaryUrl() }}" alt="preview image" width="100px" height="100px">
+                                                    <img src="{{method_exists($path_image, 'TemporaryUrl')?$path_image->TemporaryUrl():asset("storage/".$path_image)}}" alt="preview image" width="100px" height="100px">
                                                     @else
                                                     <svg class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
                                                         <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
@@ -179,13 +180,12 @@
                                         <div class="col-start-1 col-span-2">
                                             <div class="pt-2 pr-4">
                                                 <h3 class="text-center block text-sm font-medium text-gray-700">Superficie</h3>
-                                                <div class="">
-                                                    <div class="">
-                                                        <select wire:model="surface" class="text-center mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-                                                            <option value="">Superficie</option>
-                                                            @for($var=1; $var<=200;$var=$var+1)
-                                                                <option type="int" value="{{$var}}"> {{$var}} mts</option>
-                                                            @endfor
+                                                <div class="w-full border">
+                                                    <div class="flex">
+                                                        <input type="text" class=" w-full text-center mt-1 block border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" placeholder="Estado" wire:model="surface">
+                                                        <select wire:model="surface" class="w-full col-span-2 text-center mt-1 block border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                                                            <option value=""></option>
+                                                            <option type="int" value="">  mts</option>
                                                         </select>
                                                     </div>
                                                 </div>
